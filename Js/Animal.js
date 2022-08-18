a4edf6062a349a8695e2447dc432701c41eb06d3
Senu14 <makli203090@gmail.com>
2022-08-18 13:45:28 +0200

    Animal Gallery

---
+++ Js/Animal.js
@@ -0,0 +1,12 @@
+const myGallery = document.getElementById("app");
+const mySelect = document.getElementById("animals");
+
+mySelect.addEventListener("change,animalSelected");
+function animalSelected();
+
+console.log(mySelect.value);
+
+buildAnimalCard(mySelect.value);
+function buildAnimalCard(testvalue);
+    console.log(yeeeee);
+    console.log(testvalue);
